2383958b553a64f2146fa0e21fad5d4ae6567ef4
Matheus G. Rodrigues <matheusgomes1203@hotmail.com>
2023-10-04 17:41:57 -0300

    test: :white_check_mark: Testando metodos asyncronos
    Testando metodos com async/await

--- src/async/async.test.ts
+++ src/async/async.test.ts
@@ -2,9 +2,60 @@ import { fetchData } from "../test-utils";
 
 describe("Testando Código Assíncrono", () => {
   // ====================================
+
+  // Testando promisses
+
+  // ====================================
+
   it("Deve testar as Promises", () => {
     return fetchData().then((data) => {
       expect(data).toBe("peanut butter");
     });
   });
+
+  // ====================================
+
+  // Async/Await
+
+  // ====================================
+
+  describe("Deve testar as Promises com assincronas", () => {
+    // ====================================
+
+    it("Deve testar as Promises com async/await", async () => {
+      const data = await fetchData();
+      expect(data).toBe("peanut butter");
+    });
+
+    // ====================================
+
+    it("Deve testar as [exceptions] do async", async () => {
+      try {
+        await fetchData();
+      } catch (e) {
+        expect(e).toMatch("error");
+      }
+    });
+
+    // ====================================
+
+    it("Deve testar a resolução do async com [resolves]", async () => {
+      await expect(fetchData()).resolves.toBe("peanut butter");
+    });
+
+    // ====================================
+
+    it("Deve testar a resolução do async com [reject]", async () => {
+     // await expect(fetchData()).rejects.toBe("error");
+    });
+
+    // ====================================
+
+    it("Deve testar a resolução do async com [catch]", async () => {
+     // expect.assertions(1);
+     // return fetchData().catch((e) => expect(e).toMatch("error"));
+    });
+  });
+
+  // ====================================
 });
